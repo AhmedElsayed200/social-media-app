04c368885a4c621a3599b79958fc05d7d0649d70
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2023-08-21 03:50:07 +0300

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const path = require("path");
 
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
 };
+
+module.exports = nextConfig;
