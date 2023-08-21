bb6d79bb3ff2cafbbd4b62d26a6513f7b79749f3
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2023-08-21 03:56:13 +0300

    images not optimized config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
